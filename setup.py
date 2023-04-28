be61d326404d67ac11af48e5238660e324e7ac7a
sabrina.bassetto <sabrina.bassetto@ahead-research.com>
2023-04-28 16:31:55 +0200

    update setup

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(name='up_pps',
       author='ACTOR',
       author_email='ahead@ahead-research.com',
       url='https://github.com/aiplan4eu/up-pps',
-      packages=['up_pps', 'dataenv', 'core', 'manager'],
+      packages=['up_pps', 'up_pps.dataenv', 'up_pps.core', 'up_pps.manager'],
       install_requires=['pip==22.3.1', 'numpy==1.24.1', 'ortools==9.5.2237'],
       python_requires='>=3.7',
       license='APACHE'
